38f23bf1da2c48bf9062eb57e674ee45a68967ed
Wilder <wildersms@gmail.com>
2022-04-15 20:08:18 -0300

    Crear un NavBar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <NavBar/>
+      <h1 className="text-3xl font-bold underline">Hola Mi tutorsito de React </h1>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          MotoEnvios LM
         </p>
         <a
           className="App-link"
@@ -17,7 +20,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
